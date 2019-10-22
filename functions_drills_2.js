b8eea12e2c4410cdaea1e85ee9687dde2ea6d254
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-10-21 18:34:49 -0700

    coded beyond function

--- functions_drills_2.js
+++ functions_drills_2.js
@@ -3,4 +3,15 @@ function jediName (firstName, lastName){
   return lastName.slice(0,3) + firstName.slice(0,2).toLowerCase();
 }
 
+function beyond (num) {
+  let rsp = 'Staying home';
+  if (num===Infinity || num===-Infinity) rsp = 'And beyond';
+  else if (num > 0) rsp = 'To infinity';
+  else if (num < 0) rsp = 'To negative infinity';
+  return rsp;
+}
+
+
+
 console.log(jediName('Angel', 'Alicea'));
+console.log(beyond(Infinity),beyond(-Infinity),beyond(1000),beyond(-1000),beyond(0));
