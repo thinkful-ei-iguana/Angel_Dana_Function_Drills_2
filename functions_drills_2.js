ec8243f58347065e5f87ebcb0d884e0a32adef4b
Angel Alicea <ala31996@gmail.com>
2019-10-22 10:22:26 -0400

    solved rock paper scissors drill

--- functions_drills_2.js
+++ functions_drills_2.js
@@ -32,6 +32,37 @@ function daysInMonth(month, leapYear=false){
   }
   return `${month} has ${days} days.`;
 }
+
+function game (userChoice) {
+  const computerChoice = Math.floor(Math.random() * 3) + 1; 
+  if (userChoice !== 1 && userChoice !== 2 && userChoice !== 3) {
+    throw 'number has to be 1,2,3' ;
+  } else if (userChoice === computerChoice) {
+    return 'You tied!';
+  } else if (userChoice === 1 && computerChoice === 3 || userChoice === 3 && computerChoice === 2 ||userChoice === 2 && computerChoice === 1) {
+    return `${convertToString(userChoice,true)} beats ${convertToString(computerChoice,false)}. You won!` ;
+  } else {
+    return `${convertToString(userChoice,true)} beats ${convertToString(computerChoice,false)}. The Computer won!` ; 
+  }
+}
+  
+function convertToString (number,capitalize) {
+  if (number===1) {
+    return (capitalize)? 'Rock' : 'rock' ;
+  } else if (number === 2) {
+    return (capitalize)? 'Paper' : 'paper';
+  } else {
+    return (capitalize)? 'Scissors' : 'scissors';
+  }
+}
+  
+try {
+  game(1) ;
+} catch (e) {
+  console.error(e) ;
+}
+
+
 console.log(daysInMonth('February',true));
 console.log(encode('craft block argon meter bells brown croon droop'));
 console.log(jediName('Angel', 'Alicea'));
